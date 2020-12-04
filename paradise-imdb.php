a33f4f773d062b30295a299c635d4e315cfd17cc
Reza Bagheri <rezabagheri@gmail.com>
2020-12-04 17:57:41 +0330

    Add files via upload

---
+++ paradise-imdb.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package ParadiseIMDB
+ */
+/*
+Plugin Name: Paradise IMDB
+Plugin URI: https://wwwparadisecyber.com/wp/plugins/imdb
+Description: A plugin to create movie database, grab movie info from imdb
+Version: 1.0.0
+Author: Reza Bagheri( Rezo ) | <a href = "mailto: rezabagheri@gmail.com">Send Email to author</a>
+Author URI: https://www.paradisecyber.com/whoami
+License: GPL3 or later
+Text Domain: paradise-imdb
+*/
